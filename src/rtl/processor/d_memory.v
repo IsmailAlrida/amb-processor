50f9f7da8e0b1e7f3cba109abc33454fc8e9a18a
alridaossama66@gmail.com <alridaossama66@gmail.com>
2026-04-26 20:47:41 +0400

    saving imp before going crazy

--- src/rtl/processor/d_memory.v
+++ src/rtl/processor/d_memory.v
@@ -11,4 +11,18 @@ module dmem #(
     input ReadEn,
     input WriteEn
 );
+
+    reg [DATA_WORD_WIDTH-1:0] dmemory [0:DATA_ADDR_SPACE**2 - 1];
+
+    always @(*) begin
+        if (ReadEn) begin
+            DataOut = dmemory[Address];
+        end
+    end
+
+    always @(posedge clk) begin
+        if (WriteEn) begin
+            dmemory[Address] <= DataIn;
+        end 
+    end
 endmodule
